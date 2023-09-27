bd60e728b447b7a87f2668cf75a7bf2d5d6389f1
zlotfi <zlotfi758@gmail.com>
2023-09-27 22:53:39 +0330

    add register method

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,23 @@
 package service;
 
+import entity.User;
+import repository.UserRepository;
+
+import java.sql.SQLException;
+
 public class UserService {
+
+    private final UserRepository userRepository = new UserRepository();
+
+    public UserService() throws SQLException {
+    }
+
+    public void register() throws SQLException {
+        User user = new User(null,"zahra","zlotfi","zlotfi758@gmail.com","1380");
+        int result = userRepository.save(user);
+        if(result != 0)
+            System.out.println(user.getName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
 }
