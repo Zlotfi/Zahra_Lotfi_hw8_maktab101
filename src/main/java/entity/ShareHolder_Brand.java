b5319f082fa6f4ed658cec62ce5d532baea74d46
zlotfi <zlotfi758@gmail.com>
2023-09-27 20:22:02 +0330

    edit attribute

--- src/main/java/entity/ShareHolder_Brand.java
+++ src/main/java/entity/ShareHolder_Brand.java
@@ -1,4 +1,8 @@
 package entity;
 
-public class ShareHolder_Brand {
+public class ShareHolder_Brand extends ShareHolder extends Brand{
+    public ShareHolder_Brand(int brandId,int shareId) {
+        this.brandId = brandId;
+        this.shareId = shareId;
+    }
 }
