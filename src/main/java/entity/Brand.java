4a86e5999cb13dde810f290e0bcb2223a195ee09
zlotfi <zlotfi758@gmail.com>
2023-09-27 19:40:42 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/Brand.java
+++ src/main/java/entity/Brand.java
@@ -1,4 +1,47 @@
 package entity;
 
-public class Brand {
+public class Brand{
+    private Integer id;
+    private String name;
+    private String website;
+    private String description;
+
+    public Brand(Integer id, String name, String website, String description) {
+        this.id = id;
+        this.name = name;
+        this.website = website;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
