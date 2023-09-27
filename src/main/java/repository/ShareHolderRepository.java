c485c8c2e2534d2886cc69fdad9af3b55ffdf69b
zlotfi <zlotfi758@gmail.com>
2023-09-28 01:52:13 +0330

    add save method

--- src/main/java/repository/ShareHolderRepository.java
+++ src/main/java/repository/ShareHolderRepository.java
@@ -1,4 +1,28 @@
 package repository;
 
+import connection.JdbcConnection;
+import entity.ShareHolder;
+import entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ShareHolderRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public ShareHolderRepository() throws SQLException {
+    }
+
+    public int save(ShareHolder shareHolder) throws SQLException {
+        String add = "INSERT INTO shareholder(name,phoneNumber,nationalCode)VALUES(?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, shareHolder.getName());
+        preparedStatement.setInt(2, shareHolder.getPhoneNumber());
+        preparedStatement.setString(3, shareHolder.getNationalCode());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
