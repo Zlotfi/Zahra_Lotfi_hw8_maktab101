2aedc9db2f15591640114bfffbd1b0e3bc228fe5
zlotfi <zlotfi758@gmail.com>
2023-09-28 00:15:23 +0330

    add register method

--- src/main/java/service/CategoryService.java
+++ src/main/java/service/CategoryService.java
@@ -1,4 +1,24 @@
 package service;
 
+import entity.Category;
+import entity.User;
+import repository.CategoryRepository;
+
+import java.sql.SQLException;
+
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository = new CategoryRepository();
+
+    public CategoryService() throws SQLException {
+    }
+
+    public void register() throws SQLException {
+        Category category = new Category(null,"clothing","All kinds of children's, women's and men's clothes");
+        int result = categoryRepository.save(category);
+        if(result != 0)
+            System.out.println(category.getName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
 }
