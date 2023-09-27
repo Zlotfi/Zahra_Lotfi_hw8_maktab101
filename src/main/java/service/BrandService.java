1e59f46cc0f5be697aef3a2393461cb59b6ad3d5
zlotfi <zlotfi758@gmail.com>
2023-09-28 01:45:44 +0330

    add register method

--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -1,4 +1,24 @@
 package service;
 
+import entity.Brand;
+import entity.User;
+import repository.BrandRepository;
+
+import java.sql.SQLException;
+
 public class BrandService {
+
+    private final BrandRepository brandRepository = new BrandRepository();
+
+    public BrandService() throws SQLException {
+    }
+
+    public void register() throws SQLException {
+        Brand brand = new Brand(null,"zara","www.zara.com","The Zara brand is one of the largest brands of women's, men's and children's clothing in the world, which began its serious activity in Spain in 1975. This brand does not spend any money on advertising and marketing, and from the very beginning, it has spent all its energy and capital on opening different stores in all parts of the world.");
+        int result = brandRepository.save(brand);
+        if(result != 0)
+            System.out.println(brand.getName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
 }
