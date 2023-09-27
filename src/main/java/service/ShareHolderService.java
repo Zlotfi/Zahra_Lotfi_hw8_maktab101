bf878a18ebe575029a9ba6dade5216ec5d27b38e
zlotfi <zlotfi758@gmail.com>
2023-09-28 02:13:05 +0330

    add register method

--- src/main/java/service/ShareHolderService.java
+++ src/main/java/service/ShareHolderService.java
@@ -1,4 +1,25 @@
 package service;
 
+import entity.ShareHolder;
+import entity.User;
+import repository.ShareHolderRepository;
+import repository.UserRepository;
+
+import java.sql.SQLException;
+
 public class ShareHolderService {
+
+    private final ShareHolderRepository shareHolderRepository = new ShareHolderRepository();
+
+    public ShareHolderService() throws SQLException {
+    }
+
+    public void register() throws SQLException {
+        ShareHolder shareHolder = new ShareHolder(null,"Amancio Ortega","09126287878","4160983658");
+        int result = shareHolderRepository.save(shareHolder);
+        if(result != 0)
+            System.out.println(shareHolder.getName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
 }
