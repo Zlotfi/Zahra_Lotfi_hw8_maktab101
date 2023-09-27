4dfbcfad017e5604c74044c3044110de2aa25735
zlotfi <zlotfi758@gmail.com>
2023-09-27 18:58:55 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -1,4 +1,37 @@
 package entity;
 
 public class Category {
+    private Integer id;
+    private String name;
+    private String discription;
+
+    public Category(Integer id, String name, String discription) {
+        this.id = id;
+        this.name = name;
+        this.discription = discription;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
 }
