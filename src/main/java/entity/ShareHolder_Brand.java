505484b57432a5a8dd26dc7c73364a6d1c583baa
zlotfi <zlotfi758@gmail.com>
2023-09-27 22:01:57 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/ShareHolder_Brand.java
+++ src/main/java/entity/ShareHolder_Brand.java
@@ -1,8 +1,27 @@
 package entity;
 
-public class ShareHolder_Brand extends ShareHolder extends Brand{
-    public ShareHolder_Brand(int brandId,int shareId) {
-        this.brandId = brandId;
+public class ShareHolder_Brand{
+    private int shareId;
+    private int brandId;
+
+    public ShareHolder_Brand(int shareId, int brandId) {
         this.shareId = shareId;
+        this.brandId = brandId;
+    }
+
+    public int getShareId() {
+        return shareId;
+    }
+
+    public void setShareId(int shareId) {
+        this.shareId = shareId;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
     }
 }
