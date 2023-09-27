b155d848fe0fe48cddfbc3a47cd2430bb72384ca
zlotfi <zlotfi758@gmail.com>
2023-09-27 19:49:08 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/ShareHolder.java
+++ src/main/java/entity/ShareHolder.java
@@ -1,4 +1,47 @@
 package entity;
 
 public class ShareHolder {
+    private Integer id;
+    private String name;
+    private Integer phoneNumber;
+    private String nationalCode;
+
+    public ShareHolder(Integer id, String name, Integer phoneNumber, String nationalCode) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
 }
