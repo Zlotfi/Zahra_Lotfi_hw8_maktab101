6bfd29fb4e3bb8e4fd1ff245dee12ea9d8163204
zlotfi <zlotfi758@gmail.com>
2023-09-28 00:02:03 +0330

    add save method

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,4 +1,26 @@
 package repository;
 
+import connection.JdbcConnection;
+import entity.Category;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CategoryRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public CategoryRepository() throws SQLException {
+    }
+
+    public int save(Category category) throws SQLException {
+        String add = "INSERT INTO users(name,description)VALUES(?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, category.getName());
+        preparedStatement.setString(2, category.getDescription());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
