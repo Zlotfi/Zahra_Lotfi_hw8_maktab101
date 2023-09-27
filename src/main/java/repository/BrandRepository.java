9064f1e6a21762068e814ac0fe1f043ebfb6678b
zlotfi <zlotfi758@gmail.com>
2023-09-28 01:32:49 +0330

    add save method

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -1,4 +1,28 @@
 package repository;
 
+import connection.JdbcConnection;
+import entity.Brand;
+import entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BrandRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public BrandRepository() throws SQLException {
+    }
+
+    public int save(Brand brand) throws SQLException {
+        String add = "INSERT INTO brand(name,website,description)VALUES(?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, brand.getName());
+        preparedStatement.setString(2, brand.getWebsite());
+        preparedStatement.setString(3, brand.getDescription());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
