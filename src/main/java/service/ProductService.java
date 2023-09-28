744ee6bd0a8e8c75613bab8a39839eb5ffc0b42d
zlotfi <zlotfi758@gmail.com>
2023-09-28 21:13:51 +0330

    add changeProduct and delete method

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -5,19 +5,41 @@ import entity.User;
 import repository.ProductRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ProductService {
 
     private final ProductRepository productRepository = new ProductRepository();
+    Scanner scanner = new Scanner(System.in);
 
     public ProductService() throws SQLException {
     }
 
-    public void register() throws SQLException {
-        Product product = new Product(null,"Women's coat","12-11-22",null,null);
+    public void register(Product product) throws SQLException {
         int result = productRepository.save(product);
         if(result != 0)
             System.out.println(product.getName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
+
+    public void changeProduct(int id) throws SQLException {
+        System.out.println("Please enter your new name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter your new createdate:");
+        String createdate = scanner.nextLine();
+        int result = productRepository.updateProduct(id,name,createdate);
+        if(result != 0)
+            System.out.println("successfully edited to database");
+        else
+            System.out.println("OOps! :(");
+    }
+
+    public void delete(int id) throws SQLException {
+        int result = productRepository.deleteProduct(id);
+        if(result != 0)
+            System.out.println("successfully deleted from database");
         else
             System.out.println("OOps! :(");
     }
