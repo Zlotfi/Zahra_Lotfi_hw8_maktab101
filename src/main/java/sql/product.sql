ac2a6febda54915d1c1985fe73ee7ffa3861c6b7
zlotfi <zlotfi758@gmail.com>
2023-09-29 01:13:33 +0330

    delete

--- src/main/java/sql/product.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS product(
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(50)NOT NULL,
-    createDate VARCHAR(50) NOT NULL,
-    categoryId int REFERENCES category(id),
-    brandId int REFERENCES brand(id)
-);
