fda95be9e4e04f7395ea53f422c5c1d870372290
zlotfi <zlotfi758@gmail.com>
2023-09-28 20:43:02 +0330

    add enter method

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,6 +1,8 @@
 package menu;
 
+import entity.Brand;
 import entity.User;
+import service.BrandService;
 import service.UserService;
 
 import java.sql.SQLException;
@@ -10,6 +12,7 @@ public class Menu {
 
     private final Scanner scanner = new Scanner(System.in);
     private final UserService userService = new UserService();
+    private final BrandService brandService = new BrandService();
 
     public Menu() throws SQLException {
     }
@@ -23,7 +26,7 @@ public class Menu {
         int select = scanner.nextInt();
         scanner.nextLine();
         switch (select){
-            case 1 -> System.out.println("Sign in");
+            case 1 -> enter();
             case 2 -> register();
             case 3 -> System.out.println("Exit");
             default -> System.out.println("ERROR");
@@ -45,5 +48,28 @@ public class Menu {
 
         User user = new User(null, name,username,email,password);
         userService.register(user);
+    }
+
+    public void enter() throws SQLException {
+        System.out.println("Enter your username:");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter your password:");
+        String password = scanner.nextLine();
+        User user = userService.find(username);
+        if (user == null && !user.getPassword().equals(password))
+            System.out.println("you enter a username and password incorrect");
+        else {
+            System.out.println("============================================");
+            System.out.println("1- brand");
+            System.out.println("2- category");
+            System.out.println("3- product");
+            System.out.println("4- shareholder");
+            System.out.println("5- Exit");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+
+
+        }
     }
 }
