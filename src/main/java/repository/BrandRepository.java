2eb589a0728d385a6fde3ffee1663a53fcf62985
zlotfi <zlotfi758@gmail.com>
2023-09-28 20:44:17 +0330

    add updateBrand and deleteBrand method

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -6,6 +6,7 @@ import entity.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BrandRepository {
@@ -22,6 +23,25 @@ public class BrandRepository {
         preparedStatement.setString(1, brand.getName());
         preparedStatement.setString(2, brand.getWebsite());
         preparedStatement.setString(3, brand.getDescription());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
+
+    public int updateBrand(int id,String name,String website,String description) throws SQLException {
+        String query = "UPDATE brand SET (name = ?,website = ?,description = ?) WHERE (id = ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,website);
+        preparedStatement.setString(3,description);
+        preparedStatement.setInt(4,id);
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
+
+    public int deleteBrand(int id) throws SQLException {
+        String query = "DELETE FROM brand WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1,id);
         int result = preparedStatement.executeUpdate();
         return result;
     }
