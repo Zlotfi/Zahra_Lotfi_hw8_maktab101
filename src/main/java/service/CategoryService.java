9324e13de43854bb65c7a640e85a8d29b7be5af4
zlotfi <zlotfi758@gmail.com>
2023-09-28 21:00:30 +0330

    add changeCategory and delete method

--- src/main/java/service/CategoryService.java
+++ src/main/java/service/CategoryService.java
@@ -5,10 +5,12 @@ import entity.User;
 import repository.CategoryRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class CategoryService {
 
     private final CategoryRepository categoryRepository = new CategoryRepository();
+    Scanner scanner = new Scanner(System.in);
 
     public CategoryService() throws SQLException {
     }
@@ -18,6 +20,27 @@ public class CategoryService {
         int result = categoryRepository.save(category);
         if(result != 0)
             System.out.println(category.getName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
+
+    public void changeCategory(int id) throws SQLException {
+        System.out.println("Please enter your new name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter your new description:");
+        String description = scanner.nextLine();
+        int result = categoryRepository.updateCategory(id,name,description);
+        if(result != 0)
+            System.out.println("successfully edited to database");
+        else
+            System.out.println("OOps! :(");
+    }
+
+    public void delete(int id) throws SQLException {
+        int result = categoryRepository.deleteCategory(id);
+        if(result != 0)
+            System.out.println("successfully deleted from database");
         else
             System.out.println("OOps! :(");
     }
