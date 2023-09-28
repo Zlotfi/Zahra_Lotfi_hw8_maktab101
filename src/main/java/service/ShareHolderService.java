6c1d30ee73b96e8b7d6b41016b8ddfc3665a74bd
zlotfi <zlotfi758@gmail.com>
2023-09-29 01:15:37 +0330

    add changeShareHolder method

--- src/main/java/service/ShareHolderService.java
+++ src/main/java/service/ShareHolderService.java
@@ -6,19 +6,36 @@ import repository.ShareHolderRepository;
 import repository.UserRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ShareHolderService {
 
     private final ShareHolderRepository shareHolderRepository = new ShareHolderRepository();
+    Scanner scanner = new Scanner(System.in);
 
     public ShareHolderService() throws SQLException {
     }
 
-    public void register() throws SQLException {
-        ShareHolder shareHolder = new ShareHolder(null,"Amancio Ortega","09126287878","4160983658");
+    public void register(ShareHolder shareHolder) throws SQLException {
         int result = shareHolderRepository.save(shareHolder);
         if(result != 0)
             System.out.println(shareHolder.getName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
+
+    public void changeShareHolder(int shareid) throws SQLException {
+        System.out.println("Please enter your new name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter your new phoneNumber");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Please enter your new nationalCode");
+        String nationalCode = scanner.nextLine();
+        int result = shareHolderRepository.updateShareHolder(shareid,name,phoneNumber,nationalCode);
+        if(result != 0)
+            System.out.println("successfully edited to database");
         else
             System.out.println("OOps! :(");
     }
