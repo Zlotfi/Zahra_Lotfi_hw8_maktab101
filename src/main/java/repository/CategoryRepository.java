601d54c7d5a6b042c6ecef19d13334e86d34ab3d
zlotfi <zlotfi758@gmail.com>
2023-09-28 20:59:53 +0330

    add updateCategory and deleteCategory method

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -23,4 +23,22 @@ public class CategoryRepository {
         int result = preparedStatement.executeUpdate();
         return result;
     }
+
+    public int updateCategory(int id,String name,String description) throws SQLException {
+        String query = "UPDATE users SET (name = ?,description = ?) WHERE (id = ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,description);
+        preparedStatement.setInt(3,id);
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
+
+    public int deleteCategory(int id) throws SQLException {
+        String query = "DELETE FROM category WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1,id);
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
