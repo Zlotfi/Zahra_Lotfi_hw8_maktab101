5ca5a6b81d0b2459aaae428bd6de57e776f3aedd
zlotfi <zlotfi758@gmail.com>
2023-09-30 00:23:27 +0330

    fix UserRepository field and use from ApplicationContext

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,7 +1,9 @@
 package utility;
 
 import connection.JdbcConnection;
+import repository.BrandRepository;
 import repository.UserRepository;
+import service.BrandService;
 import service.UserService;
 
 import java.sql.Connection;
@@ -11,14 +13,22 @@ public class ApplicationContext {
     private static final Connection CONNECTION;
     private static final UserRepository USER_REPOSITORY;
     private static final UserService USER_SERVICE;
+    private static final BrandRepository BRAND_REPOSITORY;
+    private static final BrandService BRAND_SERVICE;
 
     static {
         CONNECTION = JdbcConnection.getConnection();
         USER_REPOSITORY = new UserRepository(CONNECTION);
         USER_SERVICE = new UserService(USER_REPOSITORY);
+        BRAND_REPOSITORY = new BrandRepository(CONNECTION);
+        BRAND_SERVICE = new BrandService(BRAND_REPOSITORY);
     }
 
     public static UserService getUserService(){
         return USER_SERVICE;
+    }
+
+    public static BrandService getBrandService(){
+        return BRAND_SERVICE;
     }
 }
