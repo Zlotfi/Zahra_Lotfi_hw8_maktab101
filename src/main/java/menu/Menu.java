ff0b43e30971c0819edc2cc8f752af161076ebc8
zlotfi <zlotfi758@gmail.com>
2023-09-29 23:08:54 +0330

    add enter method

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -2,8 +2,7 @@ package menu;
 
 import entity.Brand;
 import entity.User;
-import service.BrandService;
-import service.UserService;
+import service.*;
 
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -13,6 +12,9 @@ public class Menu {
     private final Scanner scanner = new Scanner(System.in);
     private final UserService userService = new UserService();
     private final BrandService brandService = new BrandService();
+    private final CategoryService categoryService = new CategoryService();
+    private final ProductService productService = new ProductService();
+    private final ShareHolderService shareHolderService = new ShareHolderService();
 
     public Menu() throws SQLException {
     }
@@ -60,6 +62,8 @@ public class Menu {
         if (user != null && !user.getPassword().equals(password))
             System.out.println("you enter a username and password incorrect");
         else {
+            boolean isTrue = true;
+
             System.out.println("============================================");
             System.out.println("1- brand");
             System.out.println("2- category");
@@ -68,7 +72,112 @@ public class Menu {
             System.out.println("5- Exit");
             int number = scanner.nextInt();
             scanner.nextLine();
-
+            while (isTrue){
+            switch (number){
+                case 1 -> {
+                    System.out.println("==================================");
+                    System.out.println("1- Add brand");
+                    System.out.println("2- Update brand");
+                    System.out.println("3- Delete brand");
+                    System.out.println("4- Exit");
+                    int num = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (num) {
+                        case 1 -> {
+                            assert user != null;
+                            brandService.register(user.getId());
+                        }
+                        case 2 -> {
+                            assert user != null;
+                            brandService.changeBrand(user.getId());
+                        }
+                        case 3 -> {
+                            assert user != null;
+                            brandService.delete(user.getId());
+                        }
+                        case 4 -> isTrue = false;
+                        default -> System.out.println("error");
+                    }
+                }
+                case 2 -> {
+                    System.out.println("==================================");
+                    System.out.println("1- Add category");
+                    System.out.println("2- Update category");
+                    System.out.println("3- Delete category");
+                    System.out.println("4- Exit");
+                    int num1 = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (num1){
+                        case 1 -> {
+                            assert user != null;
+                            categoryService.register(user.getId());
+                        }
+                        case 2 -> {
+                            assert user != null;
+                            categoryService.changeCategory(user.getId());
+                        }
+                        case 3 -> {
+                            assert user != null;
+                            categoryService.delete(user.getId());
+                        }
+                        case 4 -> isTrue = false;
+                        default -> System.out.println("error");
+                    }
+                }
+                case 3 -> {
+                    System.out.println("==================================");
+                    System.out.println("1- Add product");
+                    System.out.println("2- Update product");
+                    System.out.println("3- Delete product");
+                    System.out.println("4- Exit");
+                    int num2 = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (num2){
+                        case 1 -> {
+                            assert user != null;
+                            productService.register(user.getId());
+                        }
+                        case 2 -> {
+                            assert user != null;
+                            productService.changeProduct(user.getId());
+                        }
+                        case 3 -> {
+                            assert user != null;
+                            productService.delete(user.getId());
+                        }
+                        case 4 -> isTrue = false;
+                        default -> System.out.println("error");
+                    }
+                }
+                case 4 -> {
+                    System.out.println("==================================");
+                    System.out.println("1- Add shareholder");
+                    System.out.println("2- Update shareholder");
+                    System.out.println("3- Delete shareholder");
+                    System.out.println("4- Exit");
+                    int num3 = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (num3){
+                        case 1 -> {
+                            assert user != null;
+                            shareHolderService.register(user.getId());
+                        }
+                        case 2 -> {
+                            assert user != null;
+                            shareHolderService.changeShareHolder(user.getId());
+                        }
+                        case 3 -> {
+                            assert user != null;
+                            shareHolderService.delete(user.getId());
+                        }
+                        case 4 -> isTrue = false;
+                        default -> System.out.println("error");
+                    }
+                }
+                case 5 -> isTrue = false;
+                default -> System.out.println("ERROR");
+            }
+            }
 
         }
     }
