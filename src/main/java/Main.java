be522bea3ff61b8b0d957c2b99921847345fc45a
zlotfi <zlotfi758@gmail.com>
2023-09-30 01:24:57 +0330

    edit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,9 @@
 import menu.Menu;
-import service.BrandService;
-import service.ProductService;
-import service.ShareHolderService;
-import service.UserService;
-
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-//        UserService userService = new UserService();
-//        userService.register();
-//        CategoryService categoryService = new CategoryService();
-//        categoryService.register();
-//
-//        ProductService productService = new ProductService();
-//        productService.register();
-//        BrandService brandService = new BrandService();
-//        brandService.register();
-//        ShareHolderService shareHolderService = new ShareHolderService();
-//        shareHolderService.register();
-//        UserService userService = new UserService();
-//        userService.find();
-//        userService.changeName();
-//        userService.delete();
+
         Menu menu = new Menu();
         menu.publicMenu();
     }
